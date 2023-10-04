1635bb2838cff41ef44e0c2c6e4c01c8c08f07e8
marsh826 <64233750+marsh826@users.noreply.github.com>
2023-10-05 00:12:48 +0700

    4.2

---
+++ Assignment 4.2P/MazeGame/GameObject.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace MazeGame
+{
+    public abstract class GameObject : IdentifiableObject
+    {
+        string _description;
+        string _name;
+
+        public GameObject(string[] ids, string name, string desc) : base(ids)
+        {
+            _description = desc;
+            _name = name;   
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public string ShortDescription
+        {
+            get { return $"{Name} ({FirstId})"; }
+            set { _description = value; }
+        }
+
+        public virtual string FullDescription
+        {
+            get { return _description; }
+            set { _description = value; }
+        }
+    }
+}
