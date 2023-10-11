7758c0170b28d45d49ed73d34a89688a7d1d1b29
marsh826 <lean7054@gmail.com>
2023-10-11 14:26:16 +0700

    5.3C done

---
+++ Assignment 5.3C/ShapeDrawer/MyRectangle.cs
@@ -0,0 +1,81 @@
+﻿using SplashKitSDK;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ShapeDrawer
+{
+    public class MyRectangle : Shape
+    {
+        private int _width, _height;
+
+        public MyRectangle(Color clr, float x, float y, int height, int width) : base (clr)
+        {
+            _width = width;
+            _height = height;
+            X = x; 
+            Y = y;
+        }
+
+        public MyRectangle() : this(Color.Green, 0, 0, 100, 100)
+        {
+
+        }
+
+        public int Width
+        {
+            set { _width = value; }
+            get { return _width; }
+        }
+
+        public int Height
+        {
+            set { _height = value; }
+            get { return _height; }
+        }
+
+        public override void Draw()
+        {
+            SplashKit.FillRectangle(color, X, Y, _width, _height);    
+            if (Selected == true)
+            {
+                DrawOutline();
+            }
+        }
+
+        public override void DrawOutline()
+        {
+            SplashKit.DrawRectangle(Color.Black, (X - 2), (Y - 2), (Width + 4), (Height + 4));
+        }
+
+        public override bool IsAt(Point2D pt)
+        {
+            if ((X < pt.X) && (X + _width > pt.X) && (Y < pt.Y) && (Y + _height > pt.Y))
+            {
+                return true;
+            } 
+            else
+            {
+                return false;
+            }
+        }
+
+        public override void SaveTo(StreamWriter writer)
+        {
+            writer.WriteLine("Rectangle");
+            base.SaveTo(writer);
+            writer.WriteLine(Width);
+            writer.WriteLine(Height);
+        }
+
+        public override void LoadFrom(StreamReader reader)
+        {
+            base.LoadFrom(reader);
+            Width = reader.ReadInteger();
+            Height = reader.ReadInteger();
+        }
+    }
+}
