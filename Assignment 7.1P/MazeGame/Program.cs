459ea0d5437fe5c7cbab85d5cc70d6b01ec92b90
marsh826 <lean7054@gmail.com>
2023-10-26 15:03:31 +0700

    adjustment

--- Assignment 7.1P/MazeGame/Program.cs
+++ Assignment 7.1P/MazeGame/Program.cs
@@ -1,27 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using MazeGame;
-
-Console.WriteLine("Hello, World!");
-
-/*IdentifiableObject id = new IdentifiableObject(new string[] {"ID1", "iD2", "Id3", "id4", "id1" });
-IdentifiableObject id2 = new IdentifiableObject(new string[0] { });
-
-Console.WriteLine(id.FirstId);
-
-if(id2.FirstId == "")
-{
-    Console.WriteLine("id2 returned an empty string");
-}*/
-
-/*_player = new Player("Hoang An", "the comtemplator of infinity");
-sword = new Item(new string[] { "sword" }, "a bronze sword", "A short sword cast from bronze");
-shovel = new Item(new string[] { "shovel" }, "a shovel", "A durable shovel borrowed from the village");
-axe = new Item(new string[] { "axe" }, "an obsidian axe", "A axe made of obsidian");
-_player.Inventory.Put(sword);
-_player.Inventory.Put(shovel);
-_player.Inventory.Put(pickaxe);*/
-
-
 Player _player = new Player("Hoang An", "the comtemplator of infinity");
 Item sword = new Item(new string[] { "knife" }, "an obsidian knife", "A hunting knife cast from obsidian");
 Item axe = new Item(new string[] { "axe" }, "a stone axe", "An axe made of cobblestone");
